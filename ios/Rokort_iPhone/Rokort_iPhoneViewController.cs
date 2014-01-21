c9f3056bd8198b0b764a16a1b50911d190a2e3a3
Simon Kracht <skr@trifork.com>
2014-01-21 12:49:34 +0100

    UI altered to match first use case of rokort app - starting a new trip

--- ios/Rokort_iPhone/Rokort_iPhoneViewController.cs
+++ ios/Rokort_iPhone/Rokort_iPhoneViewController.cs
@@ -7,11 +7,6 @@ namespace Rokort_iPhone
 {
 	public partial class Rokort_iPhoneViewController : UIViewController
 	{
-		/// <summary>
-		/// a class-level variable that tracks the number of times the button has been clicked.
-		/// </summary>
-		protected int _numberOfTimesClicked = 0;
-		
 		public Rokort_iPhoneViewController (string nibName, NSBundle bundle) : base (nibName, bundle)
 		{
 		}
@@ -32,8 +27,7 @@ namespace Rokort_iPhone
 			
 			//---- wire up our click me button
 			this.btnClickMe.TouchUpInside += (sender, e) => {
-				this._numberOfTimesClicked++;
-				this.lblOutput.Text = "Clicked [" + this._numberOfTimesClicked.ToString() + "] times!";
+				Console.WriteLine("Turen er startet");
 			};
 
 		}
@@ -51,14 +45,5 @@ namespace Rokort_iPhone
 			// Return true for supported orientations
 			return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
 		}
-		
-		/// <summary>
-		/// This is our common action handler. Two buttons call this via an action method.
-		/// </summary>
-		partial void actnButtonClick (MonoTouch.Foundation.NSObject sender)
-		{
-			this.lblOutput.Text = "Action button " +  ((UIButton)sender).CurrentTitle + " clicked.";
-		}
-
 	}
 }
