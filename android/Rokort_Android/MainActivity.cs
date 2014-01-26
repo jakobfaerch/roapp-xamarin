06dd5739f846e5b826cf5cfd388d140dc8d92db7
Simon Kracht <simonspostkasse@gmail.com>
2014-01-26 20:28:57 +0100

    Android test and working on Emulator w. 4.0.3 (maybe former problems were related to bad emulator?). Cosmetic changes implemented to ease transition from Start Trip view to Stop Trip view

--- android/Rokort_Android/MainActivity.cs
+++ android/Rokort_Android/MainActivity.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Android.App;
 using Android.Content;
 using Android.Runtime;
+using Android.Graphics;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
@@ -20,9 +21,12 @@ namespace Rokort_Android
 		EditText editTextDistance;
 		LinearLayout startTripWrapperLayout;
 		LinearLayout stopTripWrapperLayout;
+		TextView distanceLabelTextView;
 
 		String selectedRowerId;
 		String selectedBoatId;
+
+		Color secondaryTextColor = Color.Argb(0x44,0xff,0xff,0xff);
 
 		static List<KeyValuePair<String,String>> listRowerNames = new List<KeyValuePair<String,String>> ();
 		static List<KeyValuePair<String,String>> listBoatIds = new List<KeyValuePair<String,String>> ();
@@ -45,8 +49,17 @@ namespace Rokort_Android
 			SetContentView (Resource.Layout.Main);
 			startTripWrapperLayout = FindViewById<LinearLayout> (Resource.Id.start_trip_wrapper);
 			stopTripWrapperLayout = FindViewById<LinearLayout> (Resource.Id.stop_trip_wrapper);
+			distanceLabelTextView = FindViewById<TextView> (Resource.Id.textViewDistanceLabel);
+			distanceLabelTextView.SetTextColor (secondaryTextColor);
 			button = FindViewById<Button> (Resource.Id.myButton);
 			editTextDistance = FindViewById<EditText> (Resource.Id.editTextDistance);
+			editTextDistance.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) => {
+				if (editTextDistance.Text.ToString().Equals("")) {
+					disableButton();
+				} else {
+					enableButton ();
+				}
+			};
 
 			Spinner spinnerRowerNames = FindViewById<Spinner> (Resource.Id.spinnerRowerNames);
 			spinnerRowerNames.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs> (spinnerRowerNames_ItemSelected);
@@ -64,12 +77,15 @@ namespace Rokort_Android
 			updateUi();
 
 			button.Click += async delegate {
-				button.Enabled = false;
-				button.Text = GetString(Resource.String.button_disabled_text);
+				disableButton ();
+				button.Text = GetString (Resource.String.button_disabled_text);
+
 				if (isTripStarted) {
+					stopTripWrapperLayout.Visibility = ViewStates.Invisible;
 					await rokortService.stopTrip(Convert.ToInt16(editTextDistance.Text));
 					isTripStarted = false;
 				} else {
+					startTripWrapperLayout.Visibility = ViewStates.Invisible;
 					rokortService.RowerId = selectedRowerId;
 					await rokortService.startTrip (selectedBoatId);
 					isTripStarted = true;
@@ -102,9 +118,9 @@ namespace Rokort_Android
 
 		void updateUi ()
 		{
+			enableButton ();
 			int buttonTitle = isTripStarted ? Resource.String.stop_trip : Resource.String.start_trip;
 			button.Text = GetString (buttonTitle);
-			button.Enabled = true;
 			if (!isTripStarted) {
 				startTripWrapperLayout.Visibility = ViewStates.Visible;
 				stopTripWrapperLayout.Visibility = ViewStates.Gone;
@@ -112,6 +128,18 @@ namespace Rokort_Android
 				startTripWrapperLayout.Visibility = ViewStates.Gone;
 				stopTripWrapperLayout.Visibility = ViewStates.Visible;
 			}
+		}
+
+		void disableButton ()
+		{
+			button.Enabled = false;
+			button.SetTextColor (secondaryTextColor);
+		}
+
+		void enableButton ()
+		{
+			button.Enabled = true;
+			button.SetTextColor (Android.Graphics.Color.White);
 		}
 	}
 }
